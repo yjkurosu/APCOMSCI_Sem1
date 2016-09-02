75036a3445db0a00015359a30318ac40e848490f
yjkurosu <yjkurosu@gmail.com>
2016-09-02 10:52:24 -0700

    Added Files

---
+++ APLesson_01/Lab01Art01.java
@@ -0,0 +1,12 @@
+public class Lab01Art01
+{
+	public static void main (String[]args)
+	{
+		System.out.println("H   H EEEEE L     L      OOO       W   W   OOO  RRRR  L     DDDD  !!");
+		System.out.println("H   H E     L     L     O   O      W W W  O   O R   R L     D   D !!");
+		System.out.println("HHHHH EEEEE L     L     O   O      W W W  O   O RRRR  L     D   D !!");
+		System.out.println("H   H E     L     L     O   O       W W   O   O R   R L     D   D !!");
+		System.out.println("H   H EEEEE LLLLL LLLLL  OOO        W W    OOO  R   R LLLLL DDDD  !!");
+	}
+}
+	
---
+++ APLesson_01/Lab01Art02.java
@@ -0,0 +1,15 @@
+public class Lab01Art02
+{
+	public static void main (String[]args)
+	{
+		System.out.println(" _                         ");
+		System.out.println("( \\                        ");
+		System.out.println(" ) )                       ");
+		System.out.println("( (  .-***-.  A.-.A        ");
+		System.out.println(" \\ \\/       \\/ , , \\       ");
+		System.out.println("  \\   \\     =;  t  /=      ");
+		System.out.println("   \\   l\'\'\'.  \\',--\'   ");
+        System.out.println("  /   //   l ll            ");
+		System.out.println(" /_, ) )   l_,))           ");
+		}
+}
