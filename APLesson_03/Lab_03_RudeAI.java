fb6f3cee0ff2af288cd5099170ecd804e0073b70
yjkurosu <yjkurosu@gmail.com>
2016-09-09 12:12:55 -0700

    Added Lab

---
+++ APLesson_03/Lab_03_RudeAI.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Lab_03_RudeAI
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Hi my name is RudeAI\nI'd like to ask you a few questions\nWhat is your name?");
+		String name = keyboard.nextLine();
+		System.out.println(name + "?!!! Why would anyone name a baby that?");
+		
+	}
+} 
