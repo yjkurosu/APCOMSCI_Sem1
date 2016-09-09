5b622ed2d0a84b94b9c943e498b8a6f16554db19
yjkurosu <yjkurosu@gmail.com>
2016-09-09 10:43:46 -0700

    Added Files

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,19 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 5;
+		int b = 4;
+		int product = a * b;
+		
+		System.out.println ("5 multiplied by 4 is " + product);
+		
+		String name = "Barack " + "Obama";
+		String address = "1600 " + "Pennsylvania " + "Avenue";
+		String city = "Washington, ";
+		String state = "D.C. ";
+		String zip = "20500";
+		
+		System.out.println (name +)
+	}
+}
