af070ed9135dea0225b19c6920413e2c6bddbb06
yjkurosu <yjkurosu@gmail.com>
2016-09-19 11:14:18 -0700

    Finsished Lab

---
+++ APLesson_04/Lesson_04.java
@@ -0,0 +1,8 @@
+public class Lesson_04
+{
+	public static void main(String[]args)
+	{
+		System.out.printf("%10s  %10.2f", "test",44444564.3453);
+		System.out.printf("\n%10s  %10.2f", "thisexam",989878.00);
+	}
+}
