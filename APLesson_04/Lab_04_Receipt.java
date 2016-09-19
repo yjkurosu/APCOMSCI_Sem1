23ed608166bb33c1aade402e6f2223e51bdcc0d0
yjkurosu <yjkurosu@gmail.com>
2016-09-19 12:07:17 -0700

    Finished Lab

---
+++ APLesson_04/Lab_04_Receipt.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Lab_04_Receipt
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		Lab_04_Receipt form = new Lab_04_Receipt();
+		
+		System.out.println("Please enter item 1:");
+		String item1 = kb.nextLine();
+		System.out.println("Please enter the price:");
+		double price1 = kb.nextDouble();
+		kb.nextLine();
+		System.out.println("Please enter item 2:");
+		String item2 = kb.nextLine();
+		System.out.println("Please enter the price:");
+		double price2 = kb.nextDouble();
+		kb.nextLine();
+		System.out.println("Please enter item 3:");
+		String item3 = kb.nextLine();
+		System.out.println("Please enter the price:");
+		double price3 = kb.nextDouble();
+		kb.nextLine();
+		double subtot = price1 + price2 + price3;
+		double tax = subtot * .08;
+		double tot = subtot + tax;
+		
+		System.out.println("\n\n<<<<<<<<<<__Receipt__>>>>>>>>>>\n");
+		form.format(item1, price1);
+		form.format(item2, price2);
+		form.format(item3, price3);
+		form.format("Subtotal", subtot);
+		form.format("Tax", tax);
+		form.format("Total", tot);
+	}
+	public void format(String item, double price)
+	{
+		System.out.printf("\n* %20s ........       %3.2f", item, price);
+	}
+}
--- APLesson_04/Lesson_04.java
+++ APLesson_04/Lesson_04.java
@@ -2,7 +2,23 @@ public class Lesson_04
 {
 	public static void main(String[]args)
 	{
-		System.out.printf("%10s  %10.2f", "test",44444564.3453);
-		System.out.printf("\n%10s  %10.2f", "thisexam",989878.00);
+		
+		Lesson_04 form = new Lesson_04();
+		
+		String word1 = "blaaah";
+		double number1 = 444564.3453;
+		
+		form.format(word1, number1);
+		
+		String word2 = "yesssss";
+		double number2 = 567456.7685674;
+		form.format(word2,number2);
+		
 	}
+	
+	public void format(String word, double number)
+	{
+		System.out.printf("\n%10s  %10.2f", word, number);
+	}
+	
 }
