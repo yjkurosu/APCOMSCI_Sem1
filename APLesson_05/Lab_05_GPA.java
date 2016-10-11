07db8268942fa9c863d98194c920e7ff798de8ef
yjkurosu <yjkurosu@gmail.com>
2016-10-11 12:14:38 -0700

    Strarted Lab

---
+++ APLesson_05/Lab_05_GPA.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Lab_05_GPA
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Please enter your grade for math: ");
+		String math = kb.nextLine();
+	}
+
+}
