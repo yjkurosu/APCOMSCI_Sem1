2258644040faa7ed8c2db9b2004191749ac833b4
yjkurosu <yjkurosu@gmail.com>
2016-10-17 12:11:51 -0700

    Working on Labs

---
+++ APLesson_05/Lab_05_BMICalcPt2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Lab_05_BMICalcPt2
+{
+	static double h
+	static double w
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Hi, welcome to the BMI calculator. Please enter your height in inches.");
+		Double h = kb.nextDouble();
+		System.out.println("OK, now please enter you weight in pounds.");
+		Double w = kb.nextDouble();
--- APLesson_05/Lab_05_GPA.java
+++ APLesson_05/Lab_05_GPA.java
@@ -6,8 +6,39 @@ public class Lab_05_GPA
 	{
 		Scanner kb = new Scanner(System.in);
 		
-		System.out.println("Please enter your grade for math: ");
+		System.out.println("Please enter your grade for Math: ");
 		String math = kb.nextLine();
+		System.out.println("Please enter your grade for English: ");
+		String eng = kb.nextLine();
+		System.out.println("Please enter your grade for History: ");
+		String hist = kb.nextLine();
+		System.out.println("Please enter your grade for Science: ");
+		String sci = kb.nextLine();
+		System.out.println("Please enter your grade for Art: ");
+		String art = kb.nextLine();
+		System.out.println("Please enter your grade for PE: ");
+		String pe = kb.nextLine();
+		System.out.println("Please enter your grade for Computer Science: ");
+		String comsci = kb.nextLine();
+		
+		double totGpts = calcPoints(math) + calcPoints(eng) + calcPoints(hist) + calcPoints(sci) + calcPoints(art) + calcPoints(pe) + calcPoints(comsci);
+		double gpa = totGpts / 7;
+		System.out.printf("Your GPA is %.2f\n", gpa);
+		
+		}
+	public static double calcPoints(String grade)
+	{
+		if(grade.equals("A"))
+			return 4.0;
+		if(grade.equals("B"))
+			return 3.0;
+		if(grade.equals("C"))
+			return 2.0;
+		if(grade.equals("D"))
+			return 1.0;
+		if(grade.equals("F"))
+			return 0.0;
+		else
+			return 0.0;
 	}
-
 }
