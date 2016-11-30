eb11cbf3ff0548ee8ab801f05acc22f15f530583
yjkurosu <yjkurosu@gmail.com>
2016-11-30 12:11:38 -0800

    Finished Lab_10_KeepComps

--- APLesson_10/Lab_10_ExpressionSolver.java
+++ APLesson_10/Lab_10_ExpressionSolver.java
@@ -4,11 +4,36 @@ import java.util.Arrays;
 
 public class Lab_10_ExpressionSolver
 {
-	static ArrayList<Integer> equationList;
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter an equation: ");
-		String equation = kb.nextLine();
+		String expression = kb.nextLine();
+		
+		ArrayList<String> equation = new ArrayList<>(Array.asList(expression.split(" ")));
+		System.out.println(doEquation(equation));
+	}
+	
+	public static String doEquation(ArrayList<String> equation)
+	{
+		int i = 0;
+		while(i < equation.size())
+		{
+			if(equation.get(i).equals("*") || equation.get(i).equals("/"))
+			{
+				if(equation.get(i).equals("*"))
+				{
+					equation.set(i, (Integer.parseInt(equation.get(i-1)) * (Integer.parseInt(equation.get(i+1)))));
+				}
+				else
+				{
+					equation.set(i, (Integer.parseInt(equation.get(i-1)) / (Integer.parseInt(equation.get(i+1)))));
+				}
+                eqaution.remove(i-1);
+			    equation.remove(i);
+			}
+			else
+				i++;
+		}
 	}
 }
--- APLesson_10/Lab_10_KeepComps.java
+++ APLesson_10/Lab_10_KeepComps.java
@@ -9,17 +9,30 @@ public class Lab_10_KeepComps
 	{
 		Integer [] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
 		numList = new ArrayList<Integer>(Arrays.asList(numbers));
-		gFactor(numList);
 		System.out.println(numList);
+		removePrimes();
 	}
 	
-	public static Integer gFactor(Integer n)
+	public static int gFactor(int n)
 	{
-		for(int i = 2; i <= n-1; i++)
+		for(int i = 2; i < n; i++)
 		{
-			if(n % i = 0)
+			if(n % i == 0)
 				return 1;
 		}
 		return 0;
+	}
+	
+	public static void removePrimes()
+	{
+		for(int i = 0; i < numList.size(); i++)
+		{
+			if(gFactor(numList.get(i)) == 0)
+			{
+				numList.remove(numList.get(i));
+			}
+		}
+		System.out.println(numList);
+
 	}
 }
