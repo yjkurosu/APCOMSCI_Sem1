19a5e4cc5d4c06058542e42dad578bdd3653f77c
yjkurosu <yjkurosu@gmail.com>
2017-05-19 11:23:44 -0700

    Changes

--- Simulation/src/Simulation.java
+++ Simulation/src/Simulation.java
@@ -3,17 +3,13 @@ public class Simulation
     private double shot;
     public Simulation()
     {
-<<<<<<< HEAD
-        int shot = (int)(Math.random()*100+1);
-        if(shot>shotPct)
-=======
-        shot = 0;
+        double shot = (double)(Math.random()*100+1);
     }
+
     public boolean wentIn(double shotPct)
     {
         shot = (double)(Math.random()*100+1);
         if(shot<=shotPct)
->>>>>>> a4eb2096cf18ea7c3d3233a60dc2da251cb140d4
         {
             return true;
         }
