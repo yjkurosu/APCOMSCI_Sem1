336525bcd798ed5a4adc7bf8f1c719de4f64fdfc
geffencooper <geffenc2789@gmail.com>
2017-06-07 11:42:58 -0700

    update

--- Simulation/src/SimulationRunner.java
+++ Simulation/src/SimulationRunner.java
@@ -15,19 +15,14 @@ public class SimulationRunner extends PlayerDatabase {
         Simulation s1 = new Simulation();
         PlayerDatabase HOME = new PlayerDatabase();
         PlayerDatabase AWAY = new PlayerDatabase();
-        System.out.println("Pick a home team from the following: \nWarriors \nCavaliers \nLakers, \nSpurs, \nThunder, \nBulls, \nCeltics, \nKnicks, \nRaptors, \nHeat");
+        System.out.println("Pick a home team from the following: \nWarriors \nCavaliers \nLakers \nSpurs \nThunder \nBulls \nCeltics \nKnicks \nRaptors \nHeat");
         String home = kb.nextLine();
-        System.out.println("Pick an away team from the following: Warriors, Cavaliers, Lakers, Spurs, Thunder, Bulls, Celtics, Knicks, Raptors, Heat");
+        System.out.println("Pick an away team from the following: \nWarriors\nCavaliers\nLakers\nSpurs\nThunder\nBulls\nCeltics\nKnicks\nRaptors\nHeat");
         String away = kb.nextLine();
-        String[] NBA = {"Warriors", "Cavaliers", "Lakers", "Spurs", "Thunder", "Bulls", "Celtics", "Knicks", "Raptors", "Heat"};
-        for(String team :NBA)
-        {
-            if (home.equals(team))
-            {
-                s1.gamescore(s1, HOME.returnTeam(home), AWAY.returnTeam(away));
-                System.out.println(HOME.returnTeam(home).getName() + " " + " " + s1 + AWAY.returnTeam(away).getName());
-            }
-        }
+        s1.gamescore(s1, HOME.returnTeam(home), AWAY.returnTeam(away));
+        System.out.println(HOME.returnTeam(home).getName() + "  " + s1 + "  " + AWAY.returnTeam(away).getName());
+
+
 
 
 
