a3c9545573ac149a0cb0a1bb5530449b0aba8a0c
yjkurosu <yjkurosu@gmail.com>
2017-06-09 11:15:13 -0700

    Finished Player Stats

--- Simulation/src/PlayerDatabase.java
+++ Simulation/src/PlayerDatabase.java
@@ -24,44 +24,44 @@ public class PlayerDatabase
         static Player PG4 = new Player("Jordan Clarkson", 44.5, 23.3, 32.9, 32.8);
         static Player SG4 = new Player("D'Angelo Russell", 40.5, 26.6, 35.2, 44.2);
         static Player SF4 = new Player("Brandon Ingram", 40.2, 16.8, 29.4, 27.6);
-        static Player PF4 = new Player("Julius Randle", 48.8, 21.4);
-        static Player C4 = new Player("Ivica Zubac", 52.9, 20.3);
+        static Player PF4 = new Player("Julius Randle", 48.8, 21.4, 27.0, 8.7);
+        static Player C4 = new Player("Ivica Zubac", 52.9, 20.3, 0.0, 1.6);
 
-        static Player PG5 = new Player("Tony Parker", 46.6, 20.2);
-        static Player SG5 = new Player("Danny Green", 39.2, 13.5);
-        static Player SF5 = new Player("Kawhi Leonard", 48.5, 31.1);
-        static Player PF5 = new Player("LaMArcus Aldridge", 47.7, 24.5);
-        static Player C5 = new Player("Dewayne Dedmon", 62.2, 12.3);
+        static Player PG5 = new Player("Tony Parker", 46.6, 20.2, 33.3, 12.2);
+        static Player SG5 = new Player("Danny Green", 39.2, 13.5, 37.9, 69.7);
+        static Player SF5 = new Player("Kawhi Leonard", 48.5, 31.1, 38.0, 29.4);
+        static Player PF5 = new Player("LaMArcus Aldridge", 47.7, 24.5, 41.1, 5.5);
+        static Player C5 = new Player("Dewayne Dedmon", 62.2, 12.3, 0.0, 0.0);
 
-        static Player PG6 = new Player("Rajon Rondo", 40.8, 17.8);
-        static Player SG6 = new Player("Dwayne Wade", 43.4, 29.6);
-        static Player SF6 = new Player("Jimmy Butler", 45.5, 26.5);
-        static Player PF6 = new Player("Bobby Portis", 48.8, 19.1);
-        static Player C6 = new Player("Robin Lopez", 49.3, 17.5);
+        static Player PG6 = new Player("Rajon Rondo", 40.8, 17.8, 37.6, 23.5);
+        static Player SG6 = new Player("Dwayne Wade", 43.4, 29.6, 31.0, 15.1);
+        static Player SF6 = new Player("Jimmy Butler", 45.5, 26.5, 36.7, 20.0);
+        static Player PF6 = new Player("Bobby Portis", 48.8, 19.1, 33.3, 25.4);
+        static Player C6 = new Player("Robin Lopez", 49.3, 17.5, 0.0, 0.0);
 
-        static Player PG7 = new Player("Isaiah Thomas", 46.3, 34.0);
-        static Player SG7 = new Player("Avery Bradley", 46.3, 21.9);
-        static Player SF7 = new Player("Jae Crowder", 46.3, 17.0);
-        static Player PF7 = new Player("Al Horford", 47.3, 19.8);
-        static Player C7 = new Player("Amir Johnson", 57.6, 13.6);
+        static Player PG7 = new Player("Isaiah Thomas", 46.3, 34.0, 37.9, 43.8);
+        static Player SG7 = new Player("Avery Bradley", 46.3, 21.9, 39.0, 35.5);
+        static Player SF7 = new Player("Jae Crowder", 46.3, 17.0, 39.8, 55.0);
+        static Player PF7 = new Player("Al Horford", 47.3, 19.8, 35.5, 30.5);
+        static Player C7 = new Player("Amir Johnson", 57.6, 13.6, 40.9, 17.4);
 
-        static Player PG8 = new Player("Derrick Rose", 47.1, 25.7);
-        static Player SG8 = new Player("Courtney Lee", 45.6, 14.4);
-        static Player SF8 = new Player("Carmelo Anthony", 43.3, 29.1);
-        static Player PF8 = new Player("Kristaps Porzingis", 45.0, 24.3);
-        static Player C8 = new Player("Joakim Noah", 49.3, 12.5);
+        static Player PG8 = new Player("Derrick Rose", 47.1, 25.7, 21.7, 5.9);
+        static Player SG8 = new Player("Courtney Lee", 45.6, 14.4, 40.1, 38.5);
+        static Player SF8 = new Player("Carmelo Anthony", 43.3, 29.1, 35.9, 30.3);
+        static Player PF8 = new Player("Kristaps Porzingis", 45.0, 24.3, 35.7, 32.2);
+        static Player C8 = new Player("Joakim Noah", 49.3, 12.5, 0.0, 0.0);
 
-        static Player PG9 = new Player("Kyle Lowry", 46.4, 24.9);
-        static Player SG9 = new Player("DeMar DeRozan", 46.7, 34.3);
-        static Player SF9 = new Player("DeMarre Carroll", 40.1, 15.5);
-        static Player PF9 = new Player("Serge Ibaka", 45.9, 20.9);
-        static Player C9 = new Player("Jonas Valanciunas", 55.7, 19.5);
+        static Player PG9 = new Player("Kyle Lowry", 46.4, 24.9, 41.2, 51.0);
+        static Player SG9 = new Player("DeMar DeRozan", 46.7, 34.3, 26.6, 8.1);
+        static Player SF9 = new Player("DeMarre Carroll", 40.1, 15.5, 34.1, 57.9);
+        static Player PF9 = new Player("Serge Ibaka", 45.9, 20.9, 39.8, 37.2);
+        static Player C9 = new Player("Jonas Valanciunas", 55.7, 19.5, 50.0, 0.0);
 
-        static Player PG10 = new Player("Goran Dragic", 47.5, 27.1);
-        static Player SG10 = new Player("Dion Waiters", 42.4, 26.3);
-        static Player SF10 = new Player("Rodney McGruder", 41.3, 12.3);
-        static Player PF10 = new Player("Luke Babbitt", 40.2, 13.0);
-        static Player C10 = new Player("Hassan Whiteside", 55.7, 22.7);
+        static Player PG10 = new Player("Goran Dragic", 47.5, 27.1, 40.5, 26.0);
+        static Player SG10 = new Player("Dion Waiters", 42.4, 26.3, 39.5, 32.6);
+        static Player SF10 = new Player("Rodney McGruder", 41.3, 12.3, 33.2, 47.5);
+        static Player PF10 = new Player("Luke Babbitt", 40.2, 13.0, 41.4, 75.6);
+        static Player C10 = new Player("Hassan Whiteside", 55.7, 22.7, 0.0, 0.0);
 
 
 
